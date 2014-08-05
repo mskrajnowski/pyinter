b14efd42cb203c9af7badc7fe63e420046f362fb
Marek Skrajnowski <marek.skrajnowski@10clouds.com>
2014-08-05 16:28:20 +0200

    fix: deleted the examples import

--- pyinter/__init__.py
+++ pyinter/__init__.py
@@ -6,6 +6,5 @@ arithmetic and sets of intervals (discontinuous ranges).
 from pyinter.bound import Bound
 from pyinter.interval import Interval
 from pyinter.interval_set import IntervalSet
-from pyinter import examples
 
-__all__ = ['Bound', 'Interval', 'IntervalSet', 'examples']
+__all__ = ['Bound', 'Interval', 'IntervalSet']
