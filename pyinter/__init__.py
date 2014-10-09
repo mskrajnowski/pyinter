a453125b23281886199426fe2cc6d5da561868c9
Marek Skrajnowski <marek.skrajnowski@10clouds.com>
2014-10-09 13:35:30 +0200

    Updated the pyinter.__init__ with the new operations.

--- pyinter/__init__.py
+++ pyinter/__init__.py
@@ -4,7 +4,23 @@ arithmetic and sets of intervals (discontinuous ranges).
 '''
 
 from pyinter.bound import Bound
-from pyinter.interval import Interval
+from pyinter.interval import (
+    Interval,
+    union,
+    intersection,
+    set_intersection,
+    invert,
+    difference,
+)
 from pyinter.interval_set import IntervalSet
 
-__all__ = ['Bound', 'Interval', 'IntervalSet']
+__all__ = [
+    'Bound',
+    'Interval',
+    'IntervalSet',
+    'union',
+    'intersection',
+    'set_intersection',
+    'invert',
+    'difference',
+]
